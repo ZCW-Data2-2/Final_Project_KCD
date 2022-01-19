06c00c107efe90b34588bb92d8875fe6c38b1d12
Naicker Creason <naickercreason@zipcodes-MacBook-Pro-7.local>
2022-01-19 13:40:54 -0500

    added predict model

--- arbitrary_function.py
+++ arbitrary_function.py
@@ -34,28 +34,6 @@ def get_text(data):
 
     data=data.lower()
     
-    # stopwordlist = ['a', 'about', 'above', 'after', 'again', 'ain', 'all', 'am', 'an',
-            #  'and','any','are', 'as', 'at', 'be', 'because', 'been', 'before',
-            #  'being', 'below', 'between','both', 'by', 'can', 'd', 'did', 'do',
-            #  'does', 'doing', 'down', 'during', 'each','few', 'for', 'from',
-            #  'further', 'had', 'has', 'have', 'having', 'he', 'her', 'here',
-            #  'hers', 'herself', 'him', 'himself', 'his', 'how', 'i', 'if', 'in',
-            #  'into','is', 'it', 'its', 'itself', 'just', 'll', 'm', 'ma',
-            #  'me', 'more', 'most','my', 'myself', 'now', 'o', 'of', 'on', 'once',
-            #  'only', 'or', 'other', 'our', 'ours','ourselves', 'out', 'own', 're','s', 'same', 'she', "shes", 'should', "shouldve",'so', 'some', 'such',
-            #  't', 'than', 'that', "thatll", 'the', 'their', 'theirs', 'them',
-            #  'themselves', 'then', 'there', 'these', 'they', 'this', 'those',
-            #  'through', 'to', 'too','under', 'until', 'up', 've', 'very', 'was',
-            #  'we', 'were', 'what', 'when', 'where','which','while', 'who', 'whom',
-            #  'why', 'will', 'with', 'won', 'y', 'you', "youd","youll", "youre",
-            #  "youve", 'your', 'yours', 'yourself', 'yourselves']
-
-
-     # STOPWORDS = set(stopwordlist)
-    # def cleaning_stopwords(text):
-    #     return " ".join([word for word in str(text).split() if word not in STOPWORDS])
-    # data['text'] = data['text'].apply(lambda text: cleaning_stopwords(text))
-    # data['text'].head()
 
 
 #5.2 REMOVE STOP WORDS
@@ -74,7 +52,6 @@ data=cleaning_stopwords(data)
 
 
 
-
 # 5.4 CLEAN AND REMOVE PUNCTUATION
 punctuations_list = punctuation
 
@@ -83,11 +60,6 @@ def cleaning_punctuations(data):
     return data.translate(translator)
 data=cleaning_punctuations(data)
 
-
-    
-    #def cleaning_repeating_characters(text):
-    #     return re.sub(r'(.)1+', r'1', text)
-    #data['text']=data['text'].apply(lambda x: cleaning_repeating_characters(x))
 
     
 #5.5 CLEANING NUMBERS
@@ -115,7 +87,6 @@ print(data)
 
     
 
-
 #5.8 APPLY LEMMATIZER
 
 tags = pos_tag(data)
@@ -139,71 +110,55 @@ lemmatizer = WordNetLemmatizer()
 
 print(tags,'asjdhfksjdf',data)
 
-# for t in tags:
-#     print(t)
-#     lemmatizer.lemmatize(t[0],_tag2type(t[1]))
-#     print(t)
+
 
         
 
 data = [lemmatizer.lemmatize(t[0], _tag2type(t[1])) for t in tags] 
 
 
+print(data)
 
-# #5.9 SEPERATE INPUT FEATURE AND LABEL
-# print(data)
-# X=data.text
-# Y=data.target
-
-# #above, this is not working!
+# X = data
 
 
-# type(X)
-# type(Y)
-
-# data_pos=data[data['target']==1]
-# data_neg=data[data['target']==0]
-
-# data_neg
-
-# pos_tweets=data_pos['text'].values
-# neg_tweets=data_pos['text'].values
-
-# plt.figured(figsize = (20,20))
-# wc = WordCloud(max_words = 1000 , width = 1600 , height = 800,
-#             collocations=False).generate(str(neg_tweets))
-
-# plt.figure(figsize = (20,20))
-# wc = WordCloud(max_words = 1000 , width = 1600 , height = 800,
-#             collocations=False).generate(str(pos_tweets))
-# plt.imshow(wc)
-# plt.title('Word Cloud for Positive tweets')
-
-# #5.9 SEPERATE INPUT FEATURE AND LABEL
-
-# def get_all_words(tokens_list):
-    
-#     for tokens in tokens_list:
-#         for token in tokens:
-#             yield token
-
-# all_pos_words = get_all_words(pos_tweets)
-# all_neg_words = get_all_words(neg_tweets)
-
-# freq_dist_pos = FreqDist(all_pos_words)
-# freq_dist_neg = FreqDist(all_neg_words)
+# X_train, X_test, y_train, y_test = train_test_split(X,test_size = 0.1, random_state =50)
 
 #7.1 TRANSFORMING DATASET USING TF-IDF VECTORIZER
 
+
+
 vectoriser = TfidfVectorizer(ngram_range=(1,2), max_features=500000)
-vectoriser.fit(X_train.apply(lambda x: ' '.join(x)))
+# vectoriser.fit(' '.join(data))
+vectoriser.fit(data)
+
+print('yooooo')
 
 
 
 #7.2 TRANSFORM THE DATA USING TF-IDF VECTORIZER
 
-X_train = vectoriser.transform(X_train.apply(lambda x: ' '.join(x)))
-X_test  = vectoriser.transform(X_test.apply(lambda x: ' '.join(x)))
+#data = vectoriser.transform(' '.join(data))
+data = vectoriser.transform(data)
+
+print(data)
+
+file = open('/Users/naickercreason/dev/Final_Project_KCD/ML model building/LRmodel.pickle', 'rb')
+
+info = pickle.load(file)
+
+file.close()
+
+
+# data = np.array(data)
+
+# data.reshape(-1,1)
+
+print(info.predict(data))
+
+
+# X_train = vectoriser.transform(X_train.apply(lambda x: ' '.join(x)))
+# X_test  = vectoriser.transform(X_test.apply(lambda x: ' '.join(x)))
 
 
 
